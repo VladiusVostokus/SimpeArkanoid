636d518633469e4ca98dc5bc6b6dddee19c074c8
VladiusVostokus <kovalenkov222@gmail.com>
2023-06-07 17:55:17 +0300

    Game field creating

--- game.js
+++ game.js
@@ -1,23 +1,50 @@
 'use strict';
 
-const height = 25;
-const width = 30;
+const height = 20;
+const width = 50;
+const fillSymbol = '#';
 
-const gameField = new Array(width).fill('#');
+//const gameField = new Array(width).fill('#');
 
-const updateField = (field, width) => {
+const createField = (symbol,w,h) => {
     
-    let line = '';
+    const returnArray = [];
+    const workArray = new Array(width).fill(symbol);
     
-    for(let i = 0; i < width; i ++) {
+    for(let i = 0; i < h; i++) {
         
-        line += field[i];
-        
+        returnArray.push([...workArray]);
+       
     }
     
-    console.log(line);
+    for(let i = 1; i < h - 1 ; i++) {
+        for(let j = 1; j < w - 1; j++){
+            returnArray[i][j] = ' ';
+        }
+    }
     
-}
+    return returnArray;
+    
+};
 
-updateField(gameField,width);
+
+const gameField = createField(fillSymbol,width,height);
+
+
+const updateField = (field) => {
+    
+    let str = '';
+    
+    for(const subArray of field) {
+        
+        for(const value of subArray){
+            
+            str += value;
+        }
+        console.log(str);
+        str = '';
+    }  
+};
+
+updateField(gameField,height);
 
