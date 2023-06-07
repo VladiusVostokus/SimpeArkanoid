91bb9d9ef3a9e38ccd2b9c6a225f99a9404f6ff7
VladiusVostokus <kovalenkov222@gmail.com>
2023-06-07 16:38:11 +0300

    Print array as string

--- game.js
+++ game.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log("Hello world");
+const height = 25;
+const width = 30;
+
+const gameField = new Array(width).fill('#');
+
+const updateField = (field, width) => {
+    
+    let line = '';
+    
+    for(let i = 0; i < width; i ++) {
+        
+        line += field[i];
+        
+    }
+    
+    console.log(line);
+    
+}
+
+updateField(gameField,width);
+
